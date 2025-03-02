e741546c2c3249413e0871f0dfa0fc6e6bb361c9
Babar Raza <59694837+0xlazytron@users.noreply.github.com>
2025-03-02 22:47:02 +0500

    Fixed build issues

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  distDir: "dist", // Change .next to dist (or use "build" if you prefer)
-  output: "export", // Optional: Enables static export
   images: {
-    unoptimized: true, // Required if using "export"
+    unoptimized: true, // Optional if using export mode
   },
 };
 
