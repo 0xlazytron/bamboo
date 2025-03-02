ffd9d4951302be9455d326da697e319beaed515f
Babar Raza <59694837+0xlazytron@users.noreply.github.com>
2025-03-02 21:55:41 +0500

    allowList updated

--- app/allowlist.ts
+++ app/allowlist.ts
@@ -1,4 +1,4 @@
 export const allowLists = new Map<string, Array<string>>([
-    ["WL", ["D2o4sZpo9KzaCokiEKhwSxxa74jEoHRwSTgycyCgpQHD", "6qsndNyscorDhhqAUTxpSuJvD2uYE7hYwFuMJrFx8qdJ"]],
+    ["WL", ["D2o4sZpo9KzaCokiEKhwSxxa74jEoHRwSTgycyCgpQHD", "6qsndNyscorDhhqAUTxpSuJvD2uYE7hYwFuMJrFx8qdJ", "2QtzvGAfhpe2RrFCVBCDNCGF2xJJgh6LvEjGN4xUoC8a", "E5ZiNZu8mbRHgWy4coypUCzB1PGH95Mgf8zBHC6WFCfh"]],
     ["Public", []],
 ]);
